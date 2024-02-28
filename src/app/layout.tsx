f5489f8f29473c6f3f6da17110c8ab77aaf0cd65
Kirill <sincerely.manny@gmail.com>
2024-02-28 22:13:55 +0300

    feat: added <SpeedInsights/>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/shared/Footer';
 import Header from '@/components/shared/Header';
 import '@/styles/globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { type Viewport } from 'next';
 
 import { Inter } from 'next/font/google';
@@ -56,13 +57,14 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className="from-secondary-1 min-h-screen bg-gradient-to-br to-black">
+        <html lang="ru" className="from-secondary-1 min-h-screen bg-gradient-to-br to-black">
             <body className={`font-sans ${inter.variable} min-h-screen text-white`}>
                 <div className="container mx-auto p-5">
                     <Header className="mb-10" />
                     {children}
                     <Footer />
                 </div>
+                <SpeedInsights />
             </body>
         </html>
     );
